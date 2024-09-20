98e3de9af27b8f2e0195baa6782fddca4becc87c
tomygp97 <tomasgp1997@gmail.com>
2024-09-20 20:23:39 -0300

    relationships

---
+++ relationships/app/Http/Controllers/UserController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $user = User::find(1);
+        return view('index', compact('user'));
+    }
+}
---
+++ relationships/database/seeders/PhoneSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Phone;
+
+class PhoneSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Phone::create([
+            'prefix' => 34,
+            'phone_number' => 12345678,
+            'user_id' => 1,
+        ]);
+    }
+}
